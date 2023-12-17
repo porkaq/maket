f2cd8fc1938151d5fe7fa040e6508cc8dbecf29d
porkaq <44694250+porkaq@users.noreply.github.com>
2023-12-17 14:38:27 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ async function initMap() {
     // Промис `ymaps3.ready` будет зарезолвлен, когда загрузятся все компоненты основного модуля API
     await ymaps3.ready;
 
-    const {YMap, YMapDefaultSchemeLayer} = ymaps3;
+    const YMap = ymaps3;
+    const  YMapDefaultSchemeLayer = ymaps3;
 
     // Иницилиазируем карту
     const map = new YMap(
